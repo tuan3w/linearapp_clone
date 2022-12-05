75dde0ed92f70d926992044ca9af5197d3c5a8ee
Tuan Nguyen Duc <newvalue92@gmail.com>
2022-12-05 21:14:29 +0700

    feat: PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { store } from 'store';
 
 ReactDOM.render(
@@ -22,3 +23,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
